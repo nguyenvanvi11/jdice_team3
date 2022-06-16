8841d8c09a0c166a2ca6996e39a06b3536da3594
TuyetNhung02 <107617961+TuyetNhung02@users.noreply.github.com>
2022-06-16 21:59:06 +0700

    nguyentuyetnhung

--- jdice_team3/jdice/DiceSum.java
+++ jdice_team3/jdice/DiceSum.java
@@ -21,7 +21,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
 public class DiceSum extends DieRoll {
-    private DieRoll r1
+  //  private DieRoll r1
     private DieRoll r2;
     public DiceSum(DieRoll r1
 		   DieRoll r2) {
