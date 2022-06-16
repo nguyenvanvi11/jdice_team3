48122744c57f08b721de808a798444ac76765fb5
ZanGetSu1302 <minhthinh130202@gmail.com>
2022-06-16 18:50:03 +0700

    DieRill Fix Completed

--- jdice_team3/DieRoll.java
+++ jdice_team3/DieRoll.java
@@ -20,39 +20,38 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
  */
 
-
 public class DieRoll {
-    private int ndice;
-    private int nsides;
-    private int bonus;
-    private static Random rnd;
-//    static{
-	rnd=new Random();
-    }
-    public DieRoll(int ndice,
-		   int nsides,
-		   int bonus) {
-	thisndice=ndice;
-	this.nsides=nsides;
-	this.bonus=bonus;
-    }
-    public RollResult makeRoll() {
-	RollResult r=new RollResult(bonus);
-	for(int i=0;i<ndice;i++) {
-	    int roll=rnd.nextInt(nsides)+1;
-	    r.addResult(roll)
+	private int ndice;
+	private int nsides;
+	private int bonus;
+	private static Random rnd;
+	static {
+		rnd = new Random();
 	}
-	return r;
-    }
-    public String toString() {
-	String ans =ndice+"d"+nsides;
-	if(bonus>0) {
-	    ans= ans+"+"+bonus;
+
+	public DieRoll(int ndice, int nsides, int bonus) {
+		this.ndice = ndice;
+		this.nsides = nsides;
+		this.bonus = bonus;
 	}
-	else if(bonus<0) {
-	    ans:=ans+bonus;
+
+	public RollResult makeRoll() {
+		RollResult r = new RollResult(bonus);
+		for (int i = 0; i < ndice; i++) {
+			int roll = rnd.nextInt(nsides) + 1;
+			r.addResult(roll);
+		}
+		return r;
 	}
-	return ans;
-    }
+
+	public String toString() {
+		String ans = ndice + "d" + nsides;
+		if (bonus > 0) {
+			ans = ans + "+" + bonus;
+		} else if (bonus < 0) {
+			ans = ans + bonus;
+		}
+		return ans;
+	}
 
 }
