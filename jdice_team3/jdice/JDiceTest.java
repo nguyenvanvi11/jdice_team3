fec8266f506d79cbfd51a8eb756c1d6a759c44ef
minh nguyen <nguyenanhminh168202@gmail.com>
2022-06-16 21:21:23 +0700

    add class JDiceTest

---
+++ jdice_team3/jdice/JDiceTest.java
@@ -0,0 +1,32 @@
+package jdice;
+
+import static org.junit.jupiter.api.Assertions.*;
+import java.awt.event.ActionEvent;
+import javax.swing.*;
+import org.junit.jupiter.api.Test;
+
+class JDiceTest {
+
+	private JDice createTestSubject() {
+		return new JDice();
+	}
+	
+	@Test
+	public void testJDiceListener() throws Exception {
+		JDice testSubject;
+		JList resultList = null;
+		JComboBox inputBox = null;
+	}
+	
+	@Test
+	public void testActionPerformed() throws Exception {
+		JDice testSubject;
+		ActionEvent e = null;
+	}
+	
+	@Test
+	public void testMain() throws Exception {
+		String[] args = new String[] {""};
+	}
+
+}
