2097fa30eaadcc9d70fd8fa37b8492a51e41b930
ZanGetSu1302 <minhthinh130202@gmail.com>
2022-06-16 18:48:20 +0700

    DieRoll Fix Test

--- jdice_team3/DieRoll.java
+++ jdice_team3/DieRoll.java
@@ -21,7 +21,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
 
-public class  {
+public class DieRoll {
     private int ndice;
     private int nsides;
     private int bonus;
